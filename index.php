93c7e69fc7533934ca878e0654a85eb8e303f1c3
Eric L. Hepperle <codewizard13@gmail.com>
2024-06-24 09:57:00 -0500

    UPDATE index.php:  IMAGES display correctly; missing pix noted

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
-    include_once 'includes/dbh.inc.php';
-    /**
-     * Author:          Eric Hepperle
-     * Date Created:    06/21/24
-     */
-    // Following this tut: https://www.youtube.com/watch?v=0YLJ0uO6n8I&list=PL0eyrZgxdwhwBToawjm9faF1ixePexft-&index=37&ab_channel=DaniKrossing
+include_once 'includes/dbh.inc.php';
+/**
+ * Author:          Eric Hepperle
+ * Date Created:    06/21/24
+ */
+// Following this tut: https://www.youtube.com/watch?v=0YLJ0uO6n8I&list=PL0eyrZgxdwhwBToawjm9faF1ixePexft-&index=37&ab_channel=DaniKrossing
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NO-WP: Old ES Database Tester</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
-<h1>NO-WP: Old ES Database Tester</h1>
+    <h1>NO-WP: Old ES Database Tester</h1>
 
-<?php
+    <?php
 
-// CONSTANTS
-define ("IMG_PATH", 'https://elijahstreams.com/images/faces/');
-echo "<h3>IMG_PATH: " . IMG_PATH;
+    // CONSTANTS
+    $img_path = 'https://elijahstreams.com/images/faces/';
+    echo "<h3>IMG_PATH: " . $img_path . "</h3>";
 
 
     // Get SQL query
@@ -32,40 +34,61 @@ echo "<h3>IMG_PATH: " . IMG_PATH;
     $result = mysqli_query($conn, $sql);
 
     $resultCheck = mysqli_num_rows($result);
-    
+
     // Use the data
     if ($resultCheck > 0) {
 
         ?>
         <section class="container">
-        <?php
+            <?php
 
-        while ($row = mysqli_fetch_assoc($result)) {
-            
-            echo "<div class='row'>";
-            echo "   <div class='col'>".$row['Title'] . "</div>";
-?>
-<figure>
-    <img src="" alt="">
-    <figcaption></figcaption>
-</figure>
+            while ($row = mysqli_fetch_assoc($result)) {
 
-<?php
+                echo "<div class='row'>";
+                echo "   <div class='col'>" . $row['Title'] . "</div>";
+                echo "   <div class='col'>";
 
-            echo "   <div class='col'>".$row['Title'] . "</div>";
-            
-            echo "</div>";
+                $img_url = '';
 
-        }
+                // Get working image (prefere img 1, fastest load)
+                if (isset($row['image1'])) {
+                    $img_url = $img_path . $row['image1'];
+                } elseif (isset($row['image2'])) {
+                    $img_url = $img_path . $row['image2'];
+                } elseif (isset($row['image3'])) {
+                    $img_url = $img_path . $row['image3'];
+                } else {
+                    $img_url = '<span class="error">NO IMAGE PROVIDED</span>';
+                }
 
-        ?>
+                // Get publish date
+                $date = date_create($row['Publish_Date']);
+                $pub_date = date_format($date, "l, F m, Y");
+
+                ?>
+                <figure>
+                    <img src="<?php echo $img_url; ?>">
+                    <figcaption><?php echo $row['Publish_Date']; ?></figcaption>
+                </figure>
+
+                <?php
+                echo "</div>";
+
+                echo "   <div class='col'>" . $row['Blurb'] . "</div>";
+
+                echo "</div>";
+
+            }
+
+            ?>
         </section>
         <?php
 
     }
 
 
-?>
-    
+    ?>
+
 </body>
+
 </html>
