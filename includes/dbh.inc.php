2b03affd44ad8821efb844a43bc526cefb21fd23
Eric L. Hepperle <codewizard13@gmail.com>
2024-06-24 08:34:16 -0500

    INITAL COMMIT:  NO-WP ORIG DB, New Interface

---
+++ includes/dbh.inc.php
@@ -0,0 +1,18 @@
+<?php // dbh.inc.php
+
+/**
+ * Database Handler: This file manages the connection to the database
+ */
+$host_name = 'localhost:10028';
+$database = 'dbs11290831';
+$user_name = 'root';
+$password = 'root';
+
+$conn = new mysqli($host_name, $user_name, $password, $database);
+
+if ($conn->connect_error) {
+  die('<p>Failed to connect to MySQL: ' . $conn->connect_error . '</p>');
+} else {
+  echo '<p>Connection to MySQL server successfully established.</p>';
+
+}
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+    include_once 'includes/dbh.inc.php';
+    /**
+     * Author:          Eric Hepperle
+     * Date Created:    06/21/24
+     */
+    // Following this tut: https://www.youtube.com/watch?v=0YLJ0uO6n8I&list=PL0eyrZgxdwhwBToawjm9faF1ixePexft-&index=37&ab_channel=DaniKrossing
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NO-WP: Old ES Database Tester</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<h1>NO-WP: Old ES Database Tester</h1>
+
+<?php
+    // Get SQL query
+    $sql = "SELECT * FROM videos;";
+
+    $result = mysqli_query($conn, $sql);
+
+    $resultCheck = mysqli_num_rows($result);
+    
+    // Use the data
+    if ($resultCheck > 0) {
+
+        ?>
+        <section class="container">
+        <?php
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            
+            echo "<div class='row'>" .$row['Title'] . "</div>";
+
+        }
+
+        ?>
+        </section>
+        <?php
+
+    }
+
+
+?>
+    
+</body>
+</html>
