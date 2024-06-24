ebd15dc6682ef3ae9216f99fbf43f6dffb9a7d90
Eric L. Hepperle <codewizard13@gmail.com>
2024-06-24 09:17:46 -0500

    UPDATE index.php:  DEFINE IMG_PATH

--- index.php
+++ index.php
@@ -20,6 +20,12 @@
 <h1>NO-WP: Old ES Database Tester</h1>
 
 <?php
+
+// CONSTANTS
+define ("IMG_PATH", 'https://elijahstreams.com/images/faces/');
+echo "<h3>IMG_PATH: " . IMG_PATH;
+
+
     // Get SQL query
     $sql = "SELECT * FROM videos;";
 
@@ -36,7 +42,19 @@
 
         while ($row = mysqli_fetch_assoc($result)) {
             
-            echo "<div class='row'>" .$row['Title'] . "</div>";
+            echo "<div class='row'>";
+            echo "   <div class='col'>".$row['Title'] . "</div>";
+?>
+<figure>
+    <img src="" alt="">
+    <figcaption></figcaption>
+</figure>
+
+<?php
+
+            echo "   <div class='col'>".$row['Title'] . "</div>";
+            
+            echo "</div>";
 
         }
 
