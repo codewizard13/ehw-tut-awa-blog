d1f5614427931da35423d85461acfa5d525142d5
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-03 10:30:55 -0500

    UPDATE path.php:  DISABLE debugs

--- path.php
+++ path.php
@@ -1,11 +1,16 @@
 <?php
 
+// Directory / folder path for site root
 define( 'ROOT_PATH', realpath(dirname(__FILE__)));
+// echo "<h3>ROOT_PATH: ". ROOT_PATH."</h3>";
 
+// URL for site root
 define( 'BASE_URL', "http://nowp-old-es-db.local");
+// echo "<h3>BASE_URL: ". BASE_URL."</h3>";
 
+// URL for the folder where images are stored
 define('IMG_ROOT_URL', 'https://elijahstreams.com/images/faces/');
-echo "<h3>IMG_ROOT_URL: " . IMG_ROOT_URL . "</h3>";
+// echo "<h3>IMG_ROOT_URL: " . IMG_ROOT_URL . "</h3>";
 
 /**
  * Tutorial Info:
