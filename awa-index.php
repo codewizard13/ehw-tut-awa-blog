84a161426a82cf73286f9983f2f787f21b1b98d8
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-23 10:54:55 -0500

    UPDATE awa-index.php:  ADD footer scaffold

--- awa-index.php
+++ awa-index.php
@@ -248,6 +248,18 @@ include 'inc/dbh.inc.php';
     </div>
     <!-- // PAGE WRAPPER -->
 
+
+    <!-- Footer -->
+    <div class="footer">
+        <div class="footer-content"></div>
+
+        <div class="footer-bottom">
+            &copy; erichepperle.com | Designed by Eric Hepperle
+        </div>
+    </div>
+
+    <!-- // Footer -->
+
     <!-- JQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
